15001affd58f54acfa3645ed1f5ccb86f9b01cac
Hannah Bannan <hannahbannan@Hannahs-MacBook-Pro-3.local>
2020-06-29 15:19:22 -0700

    Updates

---
+++ precache-manifest.426fd2ccf74f03c1235275dcba0df046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06bbeaf30efe81468b9a0528711bc29f",
+    "url": "/SEIR-526-MarsProject/index.html"
+  },
+  {
+    "revision": "ffcfeb18458d603d9d63",
+    "url": "/SEIR-526-MarsProject/static/css/main.14192618.chunk.css"
+  },
+  {
+    "revision": "b3351cf387104be5a601",
+    "url": "/SEIR-526-MarsProject/static/js/2.7a9d51de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SEIR-526-MarsProject/static/js/2.7a9d51de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffcfeb18458d603d9d63",
+    "url": "/SEIR-526-MarsProject/static/js/main.0593f1be.chunk.js"
+  },
+  {
+    "revision": "e4fcf2c5e51a7b2855ec",
+    "url": "/SEIR-526-MarsProject/static/js/runtime-main.35c368b3.js"
+  }
+]);
