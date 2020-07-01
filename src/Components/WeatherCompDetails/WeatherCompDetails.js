c0f5ba24c88754034c92d9731691f41ed9e2dd07
Hannah Bannan <hannahbannan@MacBook-Pro.local>
2020-07-01 12:43:27 -0700

    fixed weather comp wording

--- src/Components/WeatherCompDetails/WeatherCompDetails.js
+++ src/Components/WeatherCompDetails/WeatherCompDetails.js
@@ -5,22 +5,19 @@ import "./WeatherCompDetails.css"
 const WeatherCompDetails = ({localWeather}) => {
 
     let weatherDisplay = useContext(DataContext);
-    weatherDisplay =  Object.values(weatherDisplay).splice(0,7).reverse()
+    weatherDisplay =  Object.values(weatherDisplay).splice(0,7).reverse();
     
     let marsWeather = Math.round((weatherDisplay[0].AT.av)*1.8+32)
-    console.log(marsWeather)
 
     let localCity= localWeather.name;
-    console.log(localCity)
 
     let localWeath = Math.round(localWeather.main.temp);
-    console.log(localWeath);
 
     let compWeather = () => {
         if (marsWeather < localWeath) {
             return (<h3>Today it is <span className="em">{(localWeath-marsWeather)} degrees</span>  colder on Mars than it is in {localCity} <span className="em">({localWeath}°F)</span>.</h3>)
         } else if (marsWeather>localWeath) {
-            return (<h3>Today it is warmer on Mars than it is in {localCity}.</h3>)
+            return (<h3>Today it is <span className="em">{(marsWeather-localWeath)} degrees</span>  warmer on Mars than it is in {localCity} <span className="em">({localWeath}°F)</span>.</h3>)
         } else {
             return (<h3>Mars and {localCity} are the same temperature today!</h3>)
         } 
